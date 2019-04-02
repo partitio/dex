d971ac6de0e560bc629ef98fe196eea11916902b
Adphi <philippe.adrien.nousse@gmail.com>
2019-04-02 12:32:14 +0200

    added regiterConnector tests

--- dex/config_test.go
+++ dex/config_test.go
@@ -15,6 +15,77 @@ import (
 
 var _ = yaml.YAMLToJSON
 
+var testConfig = Config{
+	Issuer: "http://127.0.0.1:5556/dex",
+	Storage: Storage{
+		Type: "postgres",
+		Config: &sql.Postgres{
+			Host:              "10.0.0.1",
+			Port:              65432,
+			MaxOpenConns:      5,
+			MaxIdleConns:      3,
+			ConnMaxLifetime:   30,
+			ConnectionTimeout: 3,
+		},
+	},
+	Web: Web{
+		HTTP: "127.0.0.1:5556",
+	},
+	StaticClients: []storage.Client{
+		{
+			ID:     "example-app",
+			Secret: "ZXhhbXBsZS1hcHAtc2VjcmV0",
+			Name:   "Example App",
+			RedirectURIs: []string{
+				"http://127.0.0.1:5555/callback",
+			},
+		},
+	},
+	StaticConnectors: []Connector{
+		{
+			Type:   "mockCallback",
+			ID:     "mock",
+			Name:   "Example",
+			Config: &mock.CallbackConfig{},
+		},
+		{
+			Type: "oidc",
+			ID:   "google",
+			Name: "Google",
+			Config: &oidc.Config{
+				Issuer:       "https://accounts.google.com",
+				ClientID:     "foo",
+				ClientSecret: "ba$r",
+				RedirectURI:  "http://127.0.0.1:5556/dex/callback/google",
+			},
+		},
+	},
+	EnablePasswordDB: true,
+	StaticPasswords: []password{
+		{
+			Email:    "admin@example.com",
+			Hash:     []byte("$2a$10$33EMT0cVYVlPy6WAMCLsceLYjWhuHpbz5yuZxu/GAFj03J9Lytjuy"),
+			Username: "admin",
+			UserID:   "08a8684b-db88-4b73-90a9-3cd1661f5466",
+		},
+		{
+			Email:    "foo@example.com",
+			Hash:     []byte("$2a$10$33EMT0cVYVlPy6WAMCLsceLYjWhuHpbz5yuZxu/GAFj03J9Lytjuy"),
+			Username: "foo",
+			UserID:   "41331323-6f44-45e6-b3b9-2c4b60c02be5",
+		},
+	},
+	Expiry: Expiry{
+		SigningKeys:  "7h",
+		IDTokens:     "25h",
+		AuthRequests: "25h",
+	},
+	Logger: Logger{
+		Level:  "debug",
+		Format: "json",
+	},
+}
+
 func TestUnmarshalConfig(t *testing.T) {
 	rawConfig := []byte(`
 issuer: http://127.0.0.1:5556/dex
@@ -72,82 +143,11 @@ logger:
   format: "json"
 `)
 
-	want := Config{
-		Issuer: "http://127.0.0.1:5556/dex",
-		Storage: Storage{
-			Type: "postgres",
-			Config: &sql.Postgres{
-				Host:              "10.0.0.1",
-				Port:              65432,
-				MaxOpenConns:      5,
-				MaxIdleConns:      3,
-				ConnMaxLifetime:   30,
-				ConnectionTimeout: 3,
-			},
-		},
-		Web: Web{
-			HTTP: "127.0.0.1:5556",
-		},
-		StaticClients: []storage.Client{
-			{
-				ID:     "example-app",
-				Secret: "ZXhhbXBsZS1hcHAtc2VjcmV0",
-				Name:   "Example App",
-				RedirectURIs: []string{
-					"http://127.0.0.1:5555/callback",
-				},
-			},
-		},
-		StaticConnectors: []Connector{
-			{
-				Type:   "mockCallback",
-				ID:     "mock",
-				Name:   "Example",
-				Config: &mock.CallbackConfig{},
-			},
-			{
-				Type: "oidc",
-				ID:   "google",
-				Name: "Google",
-				Config: &oidc.Config{
-					Issuer:       "https://accounts.google.com",
-					ClientID:     "foo",
-					ClientSecret: "ba$r",
-					RedirectURI:  "http://127.0.0.1:5556/dex/callback/google",
-				},
-			},
-		},
-		EnablePasswordDB: true,
-		StaticPasswords: []password{
-			{
-				Email:    "admin@example.com",
-				Hash:     []byte("$2a$10$33EMT0cVYVlPy6WAMCLsceLYjWhuHpbz5yuZxu/GAFj03J9Lytjuy"),
-				Username: "admin",
-				UserID:   "08a8684b-db88-4b73-90a9-3cd1661f5466",
-			},
-			{
-				Email:    "foo@example.com",
-				Hash:     []byte("$2a$10$33EMT0cVYVlPy6WAMCLsceLYjWhuHpbz5yuZxu/GAFj03J9Lytjuy"),
-				Username: "foo",
-				UserID:   "41331323-6f44-45e6-b3b9-2c4b60c02be5",
-			},
-		},
-		Expiry: Expiry{
-			SigningKeys:  "7h",
-			IDTokens:     "25h",
-			AuthRequests: "25h",
-		},
-		Logger: Logger{
-			Level:  "debug",
-			Format: "json",
-		},
-	}
-
 	var c Config
 	if err := yaml.Unmarshal(rawConfig, &c); err != nil {
 		t.Fatalf("failed to decode config: %v", err)
 	}
-	if diff := pretty.Compare(c, want); diff != "" {
+	if diff := pretty.Compare(c, testConfig); diff != "" {
 		t.Errorf("got!=want: %s", diff)
 	}
 
--- dex/dex_test.go
+++ dex/dex_test.go
@@ -1,1 +1,43 @@
 package dex
+
+import (
+	"github.com/partitio/dex/connector/mock"
+	"github.com/partitio/dex/server"
+	"github.com/partitio/dex/storage/sql"
+	"testing"
+)
+
+func TestRegisterConnector(t *testing.T) {
+	err := RegisterConnector("name", nil)
+	if err == nil {
+		t.Error("register connector: got no error, empty config parser should fails")
+	}
+	err = RegisterConnector("ldap", func() server.ConnectorConfig {
+		return server.ConnectorConfig(nil)
+	})
+	if err == nil {
+		t.Error("register connector: got no error, pre defined connectors names should raise an error")
+	}
+	err = RegisterConnector("mock_connector", func() server.ConnectorConfig {
+		return new(mock.CallbackConfig)
+	})
+	if err != nil {
+		t.Errorf("register connector: got an unexpected error : %s", err)
+	}
+	testConfig.Storage = Storage{
+		Type: "sqlite",
+		Config: &sql.SQLite3{
+			File: ":memory:",
+		},
+	}
+	testConfig.StaticConnectors = append(testConfig.StaticConnectors, Connector{
+		Name:   "new_mock",
+		Type:   "mock_connector",
+		ID:     "new_mock",
+		Config: new(mock.CallbackConfig),
+	})
+	_, err = NewDex(testConfig)
+	if err != nil {
+		t.Errorf("initialize dex: got an unexpected error : %s", err)
+	}
+}
