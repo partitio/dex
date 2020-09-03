d646b065898308d23e9c88b753c0a875f3106602
DataHearth <antoine.l@antoine-langlois.net>
2020-09-03 15:25:34 +0200

    fix out of range error

--- connector/ldap-aggregator/server.go
+++ connector/ldap-aggregator/server.go
@@ -145,7 +145,11 @@ func (c *ldapAggregatorConnector) Update(ctx context.Context, req *UpdateRequest
 	res, err := c.LdapAggregatorDefaultServer.Update(ctx, req)
 	if err != nil {
 		// TODO: uncomment to revert changes
-		// c.ldapConnectors = append(c.ldapConnectors[:index], c.ldapConnectors[index+1:]...)
+		if len := len(c.ldapConnectors); len == index+1 {
+			// c.ldapConnectors = append(c.ldapConnectors[:index], c.ldapConnectors[index:]...)
+		} else {
+			// c.ldapConnectors = append(c.ldapConnectors[:index], c.ldapConnectors[index+1:]...)
+		}
 		return nil, err
 	}
 	res.Result.BindPW = ""
